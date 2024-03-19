dd582507a83e99e59a7e85e401d28dcd96f7e36d
billy-oneill <billy-oneill@hotmail.co.uk>
2024-03-19 16:21:54 -0500

    commit

--- Main.tf
+++ Main.tf
@@ -5,4 +5,11 @@ provider "aws" {
 resource "aws_s3_bucket" "billybucket213" {
   bucket = "billybucket213"
 }
-
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "billybucket213"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
