e8b3808814a9c25ae0bf30a843c8ed9ec4a5e9d4
billy-oneill <157623688+billy-oneill@users.noreply.github.com>
2024-03-28 21:17:44 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "bucket123" {
+resource "aws_s3_bucket" "bucket210393" {
   # Bucket configuration attributes
 }
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "bucket123"
+    bucket         = "bucket210393"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
   }
